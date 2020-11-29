1f712844e18e8138c6ea1a51e274b4421e496c7e
JoaoGabrielOliveira <joconceicao@bionexo.com>
2020-11-29 07:07:40 -0300

    Improve main tests

--- main/init.php
+++ main/init.php
@@ -1,26 +1,43 @@
 <?php
 
 use Hyper\Console;
-use Hyper\Database\CRUD\select;
-use Hyper\Database\DbConnection;
+
+use Hyper\Database\ConnectionManagement as ConnectionM;
 
 require_once dirname(__DIR__) . "/vendor/autoload.php";
 
 
-DbConnection::set_instance([
+ConnectionM::set_instance([
     'db' => [
         "driver" => "sqlite",
-        "path" => "/home/bionexo/Documents/projetos_paralelos/HyperPHP/db_operations/main/db/database.db"
+        "path" => "/home/bionexo/Documents/projetos_paralelos/PHP/db_operations/main/db/database.db"
     ]
 ]);
 
+print_r(ConnectionM::get_instance()->connect());
+
 //Demonstrate how to create or prepare your SQL string
-$stmt = DbConnection::prepare_statement("SELECT * FROM tB_cliente");
-print_r($stmt);
+//$stmt = ConnectionM::prepare_statement("SELECT * FROM tB_cliente");
+//print_r($stmt);
 
 //Using CRUD to make some operations
 
 //Select / READ
-print_r ( select::execute("tb_cliente") );
+//print_r ( select::execute("tb_cliente") );
+
+//print_r (Operations::get_all_collumns('clientes'));
+//print_r (Operations::get_all_collumns('tb_cliente'));
+/*foreach (Operations::get_all_collumns('tb_cliente') as $column)
+{
+    echo $column->name . "\n";
+}*/
+
+//print_r(Operations::create_table('t_clientes',[]));
+
+/*$result = new Result([
+    ['nome'=> 'Cleber', 'endereco_id' => '100'],['nome'=> 'Antonio', 'endereco_id' => '100']
+    ]);
+    print_r ($result->result);
+*/
 
 ?>
