cd706bf52a7380f0675b9b72f38cc0396c30ccc3
JoaoGabrielOliveira <joconceicao@bionexo.com>
2021-01-02 21:46:41 -0300

    Feat operations

--- src/operations/psql.php
+++ src/operations/psql.php
@@ -1,26 +1,62 @@
 <?php
-namespace Hyper\Record\Operation\Drivers;
+namespace Hyper\Record\Operation;
 
 use Hyper\Record\Database;
 use Hyper\Record\Operation\DatabaseOperations;
+use Hyper\Record\QueryRunner;
 
 class PostgreSQLOperations implements DatabaseOperations
 {
     public Database $database;
 
-    public function create_table(string $table_name, array $columns)
+    public function create_table(string $table_name,array ...$columns)
     {
         
+        $query = new QueryRunner;
+        $query->create()->table($table_name);
+        foreach($columns as $column)
+        {
+            $query->addColumn($column[0], $column[1], $column[2]);
+        }
+
+        try
+        {
+            $query->execute();
+        }
+        catch(Exception $e)
+        {
+            throw $e;
+        }
     }
 
     public function drop_table(string $table_name)
     {
-        
+        $query = new QueryRunner;
+        $query->drop()->table($table_name);
+
+        try
+        {
+            $query->execute();
+        }
+        catch(Exception $e)
+        {
+            throw $e;
+        }
     }
 
     public function get_all_columns(string $table_name)
     {
-        
+        $query = new QueryRunner;
+        $query->select('information_schema.columns')->where(['table_name' => $table_name]);
+
+        try
+        {
+            return $query->fetch();
+        }
+        catch(Exception $e)
+        {
+            throw $e;
+        }
     }
 }
 
--- src/operations/sqlite.php
+++ src/operations/sqlite.php
@@ -1,8 +1,11 @@
 <?php
-namespace Hyper\Database\Operations;
+namespace Hyper\Record\Operation;
 
-use Hyper\Database;
-use Hyper\Database\Drivers\DatabaseOperations;
+use Exception;
+use Hyper\Record\Connection\ConnectionManagement;
+use Hyper\Record\Database;
+use Hyper\Record\Operation\DatabaseOperations;
+use Hyper\Record\QueryRunner;
 
 class SQLiteOperations implements DatabaseOperations
 {
@@ -10,17 +13,51 @@ class SQLiteOperations implements DatabaseOperations
 
     public function get_all_columns(string $table_name)
     {
-        
+        $statement = ConnectionManagement::prepareStatement("PRAGMA table_info($table_name)");
+
+        try
+        {
+            $statement->execute();
+            return $statement->fetch();
+        }
+        catch(Exception $e)
+        {
+            throw $e;
+        }
     }
 
-    public function create_table(string $table_name, array $columns)
+    public function create_table(string $table_name,array ...$columns)
     {
+        $query = new QueryRunner;
+        $query->create()->table($table_name);
+        foreach($columns as $column)
+        {
+            $query->addColumn($column[0], $column[1], $column[2]);
+        }
 
+        try
+        {
+            $query->execute();
+        }
+        catch(Exception $e)
+        {
+            throw $e;
+        }
     }
 
     public function drop_table(string $table_name)
     {
+        $query = new QueryRunner;
+        $query->drop()->table($table_name);
 
+        try
+        {
+            $query->execute();
+        }
+        catch(Exception $e)
+        {
+            throw $e;
+        }
     }
 }
 ?>
